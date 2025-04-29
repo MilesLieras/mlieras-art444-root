768820a8b1d3b9b46c85ae6b969cb09853a275c1
MilesLieras <mlieras2644@sdsu.edu>
2025-04-28 23:17:26 -0700

    dark mode added

--- JS/script.js
+++ JS/script.js
@@ -5,3 +5,8 @@ menuToggle.addEventListener('click', () => {
   nav.classList.toggle('show-menu');
 });
 
+const toggle = document.getElementById('theme-toggle');
+
+toggle.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+});
